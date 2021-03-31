dcfe325f30ede1442789ebb3bcee7a317bd40af2
Brandon Olah <bolah1@unh.newhaven.edu>
2021-03-31 14:21:46 -0400

    Initial set-up for dice.hpp with polymorphism.

--- src/Dice.hpp
+++ src/Dice.hpp
@@ -1,6 +1,6 @@
 /*
  * Created by Marcus Novoa & Brandon Olah
- * Last Updated: Mar 8, 2021
+ * Last Updated: Mar 31, 2021
  * 
  */
 #ifndef DICE_HPP_
@@ -9,13 +9,15 @@
 #include "tools.hpp"
 #include <iostream>
 
+#define INFILE "fakeDiceInput.txt"
+
 using namespace std;
 
 class Dice {
 public:
 	Dice(int n);
-	~Dice(){delete [] diceValues;};
-	const int* roll();
+	virtual ~Dice();
+	virtual const int* roll();
 	ostream& print(ostream& out) const;
 	const int getDiceValue(int n) const {return diceValues[n];};
 
@@ -28,5 +30,24 @@ private:
 inline ostream& operator <<( ostream& out, Dice& d ) {
 	return d.print(out);
 }
+
+class CantStopDice : public Dice {
+public:
+	CantStopDice();
+	~CantStopDice();
+	const int* roll();
+private:
+	int pairValues[2] = {0,0};
+};
+
+class FakeDice : public CantStopDice {
+public:
+	FakeDice();
+	const int* roll();
+
+private:
+	ifstream diceFile;
+
+};
 
 #endif /* DICE_HPP_ */
